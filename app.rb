6775b83f8f9a67a0a42011d0ecfe124ffc5a9cdb
James Maynard & Alister A. Fenix <student@epicodus.com>
2015-01-26 14:20:07 -0800

    Add initial files.

---
+++ app.rb
@@ -0,0 +1,8 @@
+require("sinatra/activerecord")
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+# require(".lib/employee")
+# require(".lib/division")
+require("pg")
+require("pry")
